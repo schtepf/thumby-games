38e7c5d23c9e8cf3811d3ec1b4a5de7eabb3e2af
Stephanie Evert <stephanie.evert@fau.de>
2025-12-28 22:01:44 +0100

    Restarted work on shapes.py and associated demo GeomTest.py

--- lib/shapes.py
+++ lib/shapes.py
@@ -12,8 +12,8 @@
 #  - draw filled rectangle from top left (x0, y0) to bottom right (x1, y1)
 #  - according to mode, and automatically clipped to screen
 # 
-# shapes.rect_outline(x0, y0, x1, y1, col)
-#  - draw rectangle outline (1px width) in white (col != 0) or black (col = 0)
+# shapes.rect_outline(x0, y0, x1, y1, mode)
+#  - draw rectangle outline (1px width) according to mode (with outline versions ignored)
 #  - from top left (x0, y0) to bottom right (x1, y1), automatically clipped to screen
 #  - aims to be as fast as possible (unlike thumby.display.drawRectangle())
 #
@@ -41,11 +41,20 @@
 # shp.reset()
 #  - reset internal arrays shp.y1[] and shp.y2[] for defensive programming
 #
-# The core function for rendering vertical can also be used directly for maximal flexibility:
-# shapes.vline(x, y1, y2, mode)
-#  - draw vertical line from (x, y1) to (x, y2) inclusive, according to mode
-#  - automatically clipped to screen
-#  - no line is drawn if y2 < y1
+# Internal rendering functions are also available for maximal flexibility:
+#
+# shapes.vline(x1, x2, y1, y2, mode)
+#  - draw vertical line from (x, y1) to (x, y2) inclusive with call shapes.vline(x, x, y1, y2, mode)
+#  - specify x2 > x1 to draw wide line (x2 - x1 + 1 pixels) efficiently (used e.g. by rect())
+#  - automatically clipped to screen, no line is drawn if y2 < y1 or x2 < x1
+#  - draws line according to mode (fill, bg_fill, xor), outline modes are ignored
+#
+# shapes.hline(y, x1, x2, mode)
+#  - draw horizontal line from (x1, y) to (x2, y) inclusive
+#  - automatically clipped to screen, no line is drawn if x1 < x2
+#  - draws line according to mode (fill, bg_fill, xor), outline modes are ignored
+#  - this function is useful for drawing horizontal outline or grid, but inefficient for shapes
+#
 
 import thumby
 import math
@@ -57,14 +66,16 @@ bg_outline = const(4)
 xor = const(5)
 
 @micropython.viper
-def vline(x: int, y1: int, y2: int, mode: int):
+def vline(x1: int, x2: int, y1: int, y2: int, mode: int):
     scr = ptr8(thumby.display.display.buffer)
     y1_ = y1 if y1 >= 0 else 0
     y2_ = y2 if y2 < 40 else 39
+    x1_ = x1 if x1 >= 0 else 0
+    x2_ = x2 if x2 < 72 else 71
     if y2_ < y1_:
         return # no line to draw (also captures vline outside screen)
-    if not (0 <= x < 72):
-        return # line outside screen
+    if x2_ < x1_:
+        return # no line to draw (as above)
     fg = mode == fill or mode == outline
     bdry = mode == outline or mode == bg_outline
     
@@ -79,96 +90,105 @@ def vline(x: int, y1: int, y2: int, mode: int):
     if y2_byte == y1_byte:
         mask = y1_mask & y2_mask
         if mode == xor:
-            scr[y1_byte * 72 + x] ^= mask
+            for x in range(x1_, x2_ + 1):
+                scr[y1_byte * 72 + x] ^= mask
         elif fg:
-            scr[y1_byte * 72 + x] |= mask
+            for x in range(x1_, x2_ + 1):
+                scr[y1_byte * 72 + x] |= mask
         else:
-            scr[y1_byte * 72 + x] &= 0xff ^ mask
+            for x in range(x1_, x2_ + 1):
+                scr[y1_byte * 72 + x] &= 0xff ^ mask
     else:
         if mode == xor:
-            scr[y1_byte * 72 + x] ^= y1_mask
-            for yb in range(y1_byte + 1, y2_byte):
-                scr[yb * 72 + x] ^= 0xff
-            scr[y2_byte * 72 + x] ^= y2_mask
+            for x in range(x1_, x2_ + 1):
+                scr[y1_byte * 72 + x] ^= y1_mask
+                for yb in range(y1_byte + 1, y2_byte):
+                    scr[yb * 72 + x] ^= 0xff
+                scr[y2_byte * 72 + x] ^= y2_mask
         elif fg:
-            scr[y1_byte * 72 + x] |= y1_mask
-            for yb in range(y1_byte + 1, y2_byte):
-                scr[yb * 72 + x] = 0xff
-            scr[y2_byte * 72 + x] |= y2_mask
+            for x in range(x1_, x2_ + 1):
+                scr[y1_byte * 72 + x] |= y1_mask
+                for yb in range(y1_byte + 1, y2_byte):
+                    scr[yb * 72 + x] = 0xff
+                scr[y2_byte * 72 + x] |= y2_mask
         else:
-            scr[y1_byte * 72 + x] &= 0xff ^ y1_mask
-            for yb in range(y1_byte + 1, y2_byte):
-                scr[yb * 72 + x] = 0x00
-            scr[y2_byte * 72 + x] &= 0xff ^ y2_mask
+            for x in range(x1_, x2_ + 1):
+                scr[y1_byte * 72 + x] &= 0xff ^ y1_mask
+                for yb in range(y1_byte + 1, y2_byte):
+                    scr[yb * 72 + x] = 0x00
+                scr[y2_byte * 72 + x] &= 0xff ^ y2_mask
     
     if bdry: # to draw boundary, simply invert pixels at y1 and y2
-        if y1_ == y1:
-            scr[y1_byte * 72 + x] ^= 1 << y1_lsb
-        if y2_ == y2 and y1 != y2: # make sure we don't invert twice if y1 == y2
-            scr[y2_byte * 72 + x] ^= 1 << y2_msb
+        for x in range(x1_, x2_ + 1):
+            if y1_ == y1:
+                scr[y1_byte * 72 + x] ^= 1 << y1_lsb
+            if y2_ == y2 and y1 != y2: # make sure we don't invert twice if y1 == y2
+                scr[y2_byte * 72 + x] ^= 1 << y2_msb
     
-        
+@micropython.viper
+def hline(y: int, x1: int, x2: int, mode: int):
+    scr = ptr8(thumby.display.display.buffer)
+    if not (0 <= y < 40):
+        return
+    if x1 < 0:
+        x1 = 0
+    if x2 >= 72:
+        x2 = 71
+    if x2 < x1:
+        return
+    
+    y_byte_offset = (y >> 3) * 72
+    y_bit = y & 0x07
+    y_mask = 1 << y_bit
+    if mode == xor:
+        for x in range(x1, x2 + 1):
+            scr[y_byte_offset + x] ^= y_mask  # xor
+    elif mode == fill or mode == outline:
+        for x in range(x1, x2 + 1):
+            scr[y_byte_offset + x] |= y_mask  # fill
+    else:
+        y_mask = 0xff ^ y_mask
+        for x in range(x1, x2 + 1):
+            scr[y_byte_offset + x] &= y_mask  # bg_fill
+
 @micropython.viper
 def lozenge(x0: int, y0: int, size: int, mode: int):
     for i in range(size):
         j = size - i - 1
-        vline(x0 - i, y0 - j, y0 + j, mode)
+        vline(x0 - i, x0 - i, y0 - j, y0 + j, mode)
         if i > 0:
-            vline(x0 + i, y0 - j, y0 + j, mode)
+            vline(x0 + i, x0 + i, y0 - j, y0 + j, mode)
 
 @micropython.viper
 def rect(x0: int, y0: int, x1: int, y1: int, mode: int):
     bdry = mode == outline or mode == bg_outline
     if not bdry:
-        for x in range(x0, x1 + 1):
-            vline(x, y0, y1, mode)
+        vline(x0, x1, y0, y1, mode)
     else:
         bdry_mode = fill if mode == bg_outline else bg_fill
-        if x0 == x1:
-            vline(x0, y0, y1, bdry_mode)
-        else:
-            vline(x0, y0, y1, bdry_mode)
-            vline(x1, y0, y1, bdry_mode)
-            for x in range(x0 + 1, x1):
-                vline(x, y0, y1, mode)
+        fill_mode = bg_fill if mode == bg_outline else fill
+        if x0 < x1 and y0 < y1:
+            vline(x0 + 1, x1 - 1, y0 + 1, y1 - 1, fill_mode)
+        rect_outline(x0, y0, x1, y1, bdry_mode)
 
-# internal helper to draw horizontal line w/o safety checks
+# NB: buggy as long as vline() still draws the outline points
 @micropython.viper
-def _hline(x1: int, x2: int, y: int, col: int):
-    scr = ptr8(thumby.display.display.buffer)
-    y_byte_offset = (y >> 3) * 72
-    y_bit = y & 0x07
-    y_mask = 1 << y_bit
-    if col:
-        for x in range(x1, x2 + 1):
-            scr[y_byte_offset + x] |= y_mask
-    else:
-        y_mask = 0xff ^ y_mask
-        for x in range(x1, x2 + 1):
-            scr[y_byte_offset + x] &= y_mask
-
-@micropython.viper
-def rect_outline(x0: int, y0: int, x1: int, y1: int, col: int):
-    mode = fill if col else bg_fill
-    if 0 <= x0 < 72:
-        vline(x0, y0, y1, mode)
-    if 0 <= x1 < 72 and x1 != x0:
-        vline(x1, y0, y1, mode)
-    if x0 < 0:
-        x0 = 0
-    if x1 >= 72:
-        x1 = 71
-    if x0 + 1 < x1 and y0 <= y1:
-        if 0 <= y0 < 40:
-            _hline(x0, x1, y0, col)
-        if y0 < y1 < 40:
-            _hline(x0, x1, y1, col)
+def rect_outline(x0: int, y0: int, x1: int, y1: int, mode: int):
+    if x1 < x0 or y1 < y0:
+        return
+    vline(x0, x0, y0, y1, mode)
+    if x0 < x1:
+        vline(x1, x1, y0, y1, mode)
+    if x0 + 1 < x1:
+        hline(y0, x0 + 1, x1 - 1, mode)
+        if y0 < y1:
+            hline(y1, x0 + 1, x1 - 1, mode)
 
 @micropython.native
 def ellipse(x: int, y: int, rx: int, ry: int, mode: int):
     if rx < 0 or ry < 0:
         return
-    vline(x, y - ry, y + ry, mode)
+    vline(x, x, y - ry, y + ry, mode)
     if rx < 1:
         return
     bdry = mode == outline or mode == bg_outline
@@ -179,16 +199,16 @@ def ellipse(x: int, y: int, rx: int, ry: int, mode: int):
     prev_dy = ry
     for r in range(rx):
         dy = int(ratio * math.sqrt(C - dx * dx) + .5)
-        vline(x - r - 1, y - dy, y + dy, mode)
-        vline(x + r + 1, y - dy, y + dy, mode)
+        vline(x - r - 1, x - r - 1, y - dy, y + dy, mode)
+        vline(x + r + 1, x + r + 1, y - dy, y + dy, mode)
         if bdry and dy < prev_dy - 1:
-            vline(x + r, y - prev_dy, y - dy - 1, bdry_mode)
-            vline(x + r, y + dy + 1, y + prev_dy, bdry_mode)
-            vline(x - r, y - prev_dy, y - dy - 1, bdry_mode)
-            vline(x - r, y + dy + 1, y + prev_dy, bdry_mode)
+            vline(x + r, x + r, y - prev_dy, y - dy - 1, bdry_mode)
+            vline(x + r, x + r, y + dy + 1, y + prev_dy, bdry_mode)
+            vline(x - r, x - r, y - prev_dy, y - dy - 1, bdry_mode)
+            vline(x - r, x - r, y + dy + 1, y + prev_dy, bdry_mode)
         prev_dy = dy
         dx += 1.0
     if bdry:
-        vline(x + rx, y - dy, y + dy, bdry_mode)
-        vline(x - rx, y - dy, y + dy, bdry_mode)
+        vline(x + rx, x + rx, y - dy, y + dy, bdry_mode)
+        vline(x - rx, x - rx, y - dy, y + dy, bdry_mode)
         
--- playground/GeomTest.py
+++ playground/GeomTest.py
@@ -4,60 +4,92 @@ import shapes
 import textmode
 from fps import FPS
 
-
 fps = FPS()
-thumby.display.setFPS(10)
-
-while fps.time() < 2.0:
-    thumby.display.fill(0)
-    fps.tick()
-    textmode.print_text(0, 0,
-        "@ cycle\n  modes\n\n% exit", textmode.block)
-    thumby.display.update()
-
 thumby.display.setFPS(0)
 fps.tock()
-mode = 1
-
-size = 20
+page = 0
+drawmode = shapes.fill
+shape = 1 # 0 = none, 1 = rect, 2 = ellipse, 3 = lozenge, 4 = rect_outline
+speed = 3
+phase = 0
 
 while not thumby.buttonB.justPressed():
     if thumby.buttonA.justPressed():
-        mode += 1
-        if mode > 1:
-            mode = 1
-        # optional mode init code
-        fps.tock()
+        page += 1
+        if page > 1:
+            page = 0
+        fps.tock() # timing relative to page
+    elif thumby.buttonU.justPressed():
+        drawmode += 1
+        if drawmode > shapes.xor:
+            drawmode = shapes.fill
+    elif thumby.buttonL.justPressed():
+        if speed > 0:
+            speed -= 1
+    elif thumby.buttonR.justPressed():
+        if speed < 10:
+            speed += 1
+    if thumby.buttonD.justPressed():
+        shape += 1
+        if shape > 4:
+            shape = 0
 
-    thumby.display.fill(0)
+    dt = fps.frame_time()
     fps.tick()
-
-    if mode == 1:
-        if thumby.buttonU.pressed():
-            size += 1
-        elif thumby.buttonD.pressed():
-            if size > 1:
-                size -= 1
-
-        op = shapes.xor
-        if thumby.buttonL.pressed():
-            op = shapes.outline
-        elif thumby.buttonR.pressed():
-            op = shapes.bg_outline
-
-        shapes.ellipse(35, 19, size, size, shapes.bg_outline)
-        shapes.lozenge(35, 19, size, shapes.fill)
-        shapes.ellipse(15, 19, size // 4, 15, op)
-        shapes.rect(41, 10, 66, 30, op)
     
+    if page == 0:
+        thumby.display.fill(0)
+        textmode.print_text(0, 0,
+            "@ page\n% exit\n^ drawmode\n_ shape\n[] speed", 
+            textmode.block)
+        
+    elif page == 1:
+        thumby.display.fill(0)
+        for x in range(5, 72, 10):
+            shapes.vline(x, x, 0, 39, shapes.fill)
+        for y in range(5, 40, 10):
+            shapes.hline(y, 0, 71, shapes.xor)
+        
+        phase += speed * 0.5 * dt
+        if phase > math.pi:
+            phase -= math.pi # keep phase accurate in long-running demo
+        size1 = 50 * math.fabs(math.sin(phase))
+        size1_1 = int(size1)
+        size1_2 = int(size1 / 2)
+        size1_4 = int(size1 / 4)
+        size2 = 50 * math.fabs(math.cos(phase))
+        size2_1 = int(size2)
+        size2_2 = int(size2 / 2)
+        size2_4 = int(size2 / 4)
+        
+        if shape == 0:
+            pass
+        elif shape == 1:
+            shapes.rect(15 - size1_2, 19 - size1_4, 15 + size1_2, 19 + size1_4, drawmode)
+            shapes.rect(35 - size2_2, 19 - size2_2, 35 + size2_2, 19 + size2_2, drawmode)
+            shapes.rect(55 - size1_4, 19 - size1_2, 55 + size1_4, 19 + size1_2, drawmode)
+        elif shape == 2:
+            shapes.ellipse(15, 19, size1_2, size1_4, drawmode)
+            shapes.ellipse(35, 19, size2_2, size2_2, drawmode)
+            shapes.ellipse(55, 19, size1_4, size1_2, drawmode)
+        elif shape == 3:
+            shapes.lozenge(15, 19, size1_2, drawmode)
+            shapes.lozenge(35, 19, size2_1, drawmode)
+            shapes.lozenge(55, 19, size1_2, drawmode)
+        elif shape == 4:
+            shapes.rect_outline(15 - size1_2, 19 - size1_4, 15 + size1_2, 19 + size1_4, drawmode)
+            shapes.rect_outline(35 - size2_2, 19 - size2_2, 35 + size2_2, 19 + size2_2, drawmode)
+            shapes.rect_outline(55 - size1_4, 19 - size1_2, 55 + size1_4, 19 + size1_2, drawmode)
+        else:
+            raise Exception("INTERNAL ERROR")
+
         cur_fps = fps.fps()
         textmode.print_text(3, 4, f"{cur_fps:5.1f}/s", textmode.overlay)
-        textmode.print_text(0, 0, f"{size:3d} px", textmode.overlay)
         if fps.tock_time() < 4:
             textmode.print_text(0, 0, 
-                "mode 1\n\n^/_ size  \n[ outline \n] inverted", textmode.block)
+                "page 1", textmode.block)
 
     else:
-        raise Exception("FUCK")
+        raise Exception("INTERNAL ERROR")
 
     thumby.display.update()
